c882c5b2bd1b31faf78f515ee5c4de1d25e3e42c
ump <ump@localhost>
2019-11-15 15:00:31 +0700

    Second

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,20 @@
 package application;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class Main extends Application{
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
-
+	}	
 }
+	
+	
+
