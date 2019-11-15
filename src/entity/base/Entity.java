10cb1306d478beb1b517fd7594ca1a7d692b1c9c
ump <ump@localhost>
2019-11-15 14:59:58 +0700

    Second

--- src/entity/base/Entity.java
+++ src/entity/base/Entity.java
@@ -1,5 +1,13 @@
 package entity.base;
 
 public abstract class Entity {
-
+	public Entity() {
+		
+	}
+	
+	public abstract void setGc();
+	
+	public abstract void setSpawnPosition();
+	
+	public abstract void move();
 }
