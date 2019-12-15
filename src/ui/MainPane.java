ed2e8430a182b3940baa9a65946b0bec339bb40d
originalchi <original_chi@hotmail.com>
2019-12-15 12:01:54 +0700

    Update

--- src/ui/MainPane.java
+++ src/ui/MainPane.java
@@ -2,17 +2,23 @@ package ui;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import resloader.Resloader;
 
-public class MainPane {
+public class MainPane extends VBox{
 
 	private Button playButton;
 	private Button howToPlayButton;
 	private Button exitButton;
 	
 	public  MainPane() {
-
+		
+		super();
+		this.setPrefWidth(200);
+		this.setSpacing(15);
+		this.setAlignment(Pos.CENTER);
 		playButton = new Button("Play");
 		playButton.setGraphic(Resloader.play);
 		
@@ -21,6 +27,8 @@ public class MainPane {
 		
 		exitButton = new Button("Exit");
 		exitButton.setGraphic(Resloader.exit);
+		
+		this.getChildren().addAll(playButton, howToPlayButton, exitButton);
 		
 	}
 
