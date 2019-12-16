460c40d907151456a60cabe746f1d4318b80bb38
ump <ump@localhost>
2019-12-16 22:32:23 +0700

    8;p

--- src/resloader/Resloader.java
+++ src/resloader/Resloader.java
@@ -2,6 +2,7 @@ package resloader;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
@@ -23,21 +24,20 @@ public class Resloader {
 	public static ImageView howToPlay;
 	public static ImageView exit;
 	
-	public static MediaPlayer menuBackgroundMusic;
+	public static AudioClip menuBackgroundMusic;
+	public static AudioClip bombSound;
 
 	public static void LOAD() {
-
 		movingSubmarine1 = new ImageView(ClassLoader.getSystemResource("idle1.png").toString());
 		movingSubmarine2 = new ImageView(ClassLoader.getSystemResource("idle2.png").toString());
 		movingSubmarine3 = new ImageView(ClassLoader.getSystemResource("idle3.png").toString());
 		movingSubmarine4 = new ImageView(ClassLoader.getSystemResource("idle4.png").toString());
+		
 		missile = new Image(ClassLoader.getSystemResource("missile.png").toString());
 
 		enemy = new Image(ClassLoader.getSystemResource("missile.png").toString());
 
 		background = new Image(ClassLoader.getSystemResource("bg.jpg").toString());
-		
-		//menuBackgroundMusic = new MediaPlayer(new Media(ClassLoader.getSystemResource("MenuBackgroundMusic.mp3").toString()));
 
 	}
 
